0a001276ca59dc4fbdb1d5b117ad1cc46991d3df
rafaelrr1 <jrojasrr@outlook.com>
2023-05-12 10:01:14 -0500

    add componets sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './App.scss';
+import Home from './componets/home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
---
+++ src/componets/home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+ const Home = ()=>{
+    return React(
+        <div className="home">
+            <h2>Hello word!!</h2>
+        </div>
+    );
+ }
+ export default Home;
