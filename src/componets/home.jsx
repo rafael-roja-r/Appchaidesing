3ac9929a21be9aca32d7386862b9f2376226b72b
rafael-roja-r <jrojasrr@outlook.com>
2023-05-24 21:13:18 -0500

    update sass y icons

--- src/componets/home.jsx
+++ src/componets/home.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
-import hom from "../img/home.svg";
-import mss from "../img/messages.svg";
-import usr from "../img/user.svg";
-import est from "../img/estado.svg";
+import lo from "../img/logo/Logo.svg";
+import gr from "../img/icons-v/group.svg";
+import he from "../img/icons-v/help.svg";
+import ho from "../img/icons-v/ho.svg";
+import st from "../img/icons-v/stt.svg";
+import wh from "../img/icons-v/Wrh.svg";
+
 import Hm from "../pages/Home";
 import Ms from "../pages/Messages";
 import Us from "../pages/User";
 import Ee from "../pages/Estado";
 
 
- const Home = ()=>{
+const Home = () => {
 
-    const [open, Start] = useState('home');
+  const [open, Start] = useState('home');
 
   const Show = (option) => {
     Start(option);
@@ -19,58 +22,85 @@ import Ee from "../pages/Estado";
   };
 
 
-    return (
-      <div className="home">
-        <div className="home__container">
-          <div className="home__container__left">
-            <h2>informacion</h2>
-            {open === "home" && (
-              <div className="inicontainer">
-                <h3>Home</h3>
-                <Hm/>
-              </div>
-            )}
+  return (
+    <div className="home">
+      <div className="home__nav">
+      <div className="home__logo">
+        <img src={lo} alt="Logo de chaidising" title='Logo de chaidising' />
+      </div>
 
-            {open === "message" && (
-              <div className="inicontainer">
-                <h3>Message</h3>
-                <Ms/>
-              </div>
-            )}
+      <div className="home__nav__v">
+          <button onClick={() => Show("home")}>
+            {" "}
+            <img src={ho} alt="" />{" "}
+          </button>
+          <button onClick={() => Show("team")}>
+            <img src={gr} alt="" />{" "}
+          </button>
+          <button onClick={() => Show("warehouse")}>
+            {" "}
+            <img src={wh} alt="" />{" "}
+          </button>
+          <div className="home__buton_btn">
+          <button  onClick={() => Show("help")}>
+            {" "}
+            <img src={he} alt="" />{" "}
+          </button>
 
-            {open === "estado" && (
-              <div className="inicontainer">
-                <h3>Estado</h3>
-                <Ee/>
-              </div>
-            )}
-
-            {open === "user" && (
-              <div className="inicontainer">
-                <h3>User</h3>
-                <Us/>
-              </div>
-            )}
+          <button  onClick={() => Show("setting")}>
+            {" "}
+            <img src={st} alt="" />{" "}
+          </button>
           </div>
-          <div className="home__container__right">
-            <button onClick={() => Show("home")}>
-              {" "}
-              <img src={hom} alt="" />{" "}
-            </button>
-            <button  onClick={() => Show("message")}>
-              <img src={mss} alt="" />{" "}
-            </button>
-            <button  onClick={() => Show("estado")}>
-              {" "}
-              <img src={est} alt="" />{" "}
-            </button>
-            <button  onClick={() => Show("user")}>
-              {" "}
-              <img src={usr} alt="" />{" "}
-            </button>
-          </div>
+          
         </div>
       </div>
-    );
- }
+      
+      <div className="home__container">
+        <div className="home__nav_h">
+
+        </div>
+
+
+        <div className="home__container__left">
+          {open === "home" && (
+            <div className="inicontainer">
+              <h3>Home</h3>
+              <Hm />
+            </div>
+          )}
+
+          {open === "team" && (
+            <div className="inicontainer">
+              <h3>Team</h3>
+              <Ms />
+            </div>
+          )}
+
+          {open === "warehouse" && (
+            <div className="inicontainer">
+              <h3>Warehouse</h3>
+              <Ee />
+            </div>
+          )}
+
+          {open === "help" && (
+            <div className="inicontainer">
+              <h3>Helpr</h3>
+              <Us />
+            </div>
+          )}
+
+          {open === "setting" && (
+            <div className="inicontainer">
+              <h3>Setting</h3>
+              <Us />
+            </div>
+          )}
+        </div>
+        
+      </div>
+    </div>
+  );
+}
 export default Home;
